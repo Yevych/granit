b52254cd973024f2ede420059ff114312aae27c4
Yevych <bohdan.yevych@gmail.com>
2019-08-26 12:17:14 +0300

    add stock section and btn hovers

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
+  //Height blocks
+  function setEqualHeight (columns) {
+    var tallestcolumn = 0;
+    columns.each(function () {
+      currentHeight = $(this).height();
+      if (currentHeight > tallestcolumn) {
+        tallestcolumn = currentHeight;
+      }
+    })
+    columns.height(tallestcolumn);
+  }
 
+  setEqualHeight($('.stock__items'));
+  /*make blocks of the same height*/
+  setEqualHeight($('.stock__item'));
 });
