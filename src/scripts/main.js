d5235d52073ab1136f282f42535706bfeb478d4d
Yevych <bohdan.yevych@gmail.com>
2019-08-31 17:03:58 +0300

    add slider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,5 +5,19 @@ $(document).ready(function() {
     $(this).toggleClass('open-burger')
     $('.main-nav__list').slideToggle(500)
   })
-  
+
+  //Hero slider
+  $('.hero-slider').slick({
+    dots: false,
+    arrows: false,
+    draggable: false,
+    //pauseOnHover: false,
+    autoplay: true,
+    infinite: true,
+    fade: true,
+    speed: 2000,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  })
+
 });
